dadb577be4c4e78382f0fe963fe38de276e26ff3
kedomonzter <khemchay@gmail.com>
2018-04-26 10:41:10 +0200

    add error catching from http

--- index.js
+++ index.js
@@ -13,20 +13,27 @@ document.addEventListener('DOMContentLoaded',function() {
     document.getElementById('gh-user').childNodes[0].oninput=fetchGithub;
 },false);
 
-function fetchGithub(event){
+function fetchGithub(event) {
     let value = event.target.value || 'k';
-    if(value !== undefined){
-        fetch(`https://api.github.com/search/users?q=${value}&per_page=10&access_token=f9fe6934ede3ce06eb3cfecbad22bd13d38392f1`).then(function(response) {
-            return response.json();
-        }).then(function(githubData) {
+    let token = 'a040a1ab8bc603e3efa8947450e59f5b9feca5c9';
+    if (value !== undefined) {
+        fetch(`https://api.github.com/search/users?q=${value}&per_page=10&access_token=${token}`).then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('Network response was not ok.');
+        }).then(function (githubData) {
             var fetchedUsers = githubData.items;
             var githubDataEl = githubElement.options;
             var ghUsers = githubData.items.map(user => ({
                     text: user.login,
                     value: user.login,
-                }));
+                })
+            )
+            ;
             githubDataEl.ghUsers = ghUsers;
-
+        }).catch(function (error) {
+            console.log('There has been a problem with your fetch operation please change some query params: ', error.message);
         });
     }
 }
@@ -46,7 +53,6 @@ var ghUsers = githubUser.map(user => ({
         value: user.login,
     }));
 
-console.log('githubUser',githubUser);
 // Github Users
  var githubElement = new Autocomplete(document.getElementById('gh-user'), {
    ghUsers,
